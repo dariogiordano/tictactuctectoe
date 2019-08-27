fca6e5c2826ade13848b562c3a0fbe28d4cd30e0
TXT - Giordano Dario <giordano.dario@gcvnet.it>
2019-08-27 14:38:35 +0200

    changed css

--- src/components/CopyToClipboard/styled/index.js
+++ src/components/CopyToClipboard/styled/index.js
@@ -1,43 +1,22 @@
 import styled from "styled-components";
 
 const styledCopyToClipboard = styled.div`
-display:flex;
+
 input{
   margin: 0 0 10px 0;
   outline: 0;
   vertical-align: middle;
   background: none;
   border: 0;
-  cursor: pointer;
+  display:block;
   font-size:16px;
   width:320px;
   padding: 0;
   font-weight:bold
 }
 button{
+  display:block;
     cursor:pointer;
-}
-  &.flash {
-   
-    animation: flash 1s ease-out;
-    animation-iteration-count: 1;
-  }
-
-  
-  @keyframes flash {
-    0% { 
-      background-color: #ffffff; 
-    }
-    10% { 
-      background-color: #00ff00; 
-    }        
-    100% {
-      background-color: #ffffff;
-    }
-  }
-
-  
-  
-`;
+}`;
 
 export default styledCopyToClipboard;
